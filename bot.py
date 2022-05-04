9f4b61f10fc8167f8b8d7ed9d0eeaa290ea9dd7e
amir <amirtagashov@gmail.com>
2022-05-04 12:41:10 +0500

    first commit

---
+++ bot.py
@@ -0,0 +1,35 @@
+import os
+import telebot
+import yadisk
+
+
+from utils import BOTTOKEN, DISKTOKEN
+
+
+bot = telebot.TeleBot(BOTTOKEN)
+disk = yadisk.YaDisk(DISKTOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def echo(message):
+    bot.reply_to(message, "Hi! I'm English File Elementary 4e audio bot. Send me the number of audio you need!")
+
+
+@bot.message_handler(content_types=['text', ])
+def main(message):
+    csvlogger(str(essage.chat.id), message.from_user.first_name, message.text)
+    
+
+
+
+def csvlogger(column1: str, column2: str, column3: str):
+    columns = [column1, column2, column3]
+    with open('log.csv', 'a', encoding='utf-8') as f:
+        f.write(','.join(columns)+'\n')
+
+
+
+
+
+if __name__ == '__main__':
+    bot.polling()
